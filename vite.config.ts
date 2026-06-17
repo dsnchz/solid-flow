9fd0e078d51732cef823977047205f5a9e8963e8
Daniel Sanchez <dsanc89@icloud.com>
2026-06-17 12:47:26 -0400

    chore: update dependencies

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,12 @@
 import path from "path";
-import { defineConfig as defineViteConfig, mergeConfig } from "vite";
 import solidPlugin from "vite-plugin-solid";
-import { configDefaults, defineConfig as defineVitestConfig } from "vitest/config";
+import { configDefaults, defineConfig, mergeConfig } from "vitest/config";
 
 // We exclude the server directory because we use Bun (not Vitest) to run those tests
 const TEST_EXCLUDES = [...configDefaults.exclude, "src/index.tsx", "src/mocks", "public", "server"];
 const COVERAGE_EXCLUDE = [...TEST_EXCLUDES, "**/*.test.{ts,tsx}"];
 
-const viteConfig = defineViteConfig({
+const viteConfig = defineConfig({
   plugins: [solidPlugin()],
   server: {
     port: 3000,
@@ -27,14 +26,13 @@ const viteConfig = defineViteConfig({
   },
 });
 
-const vitestConfig = defineVitestConfig({
+const vitestConfig = defineConfig({
   test: {
     globals: true,
     environment: "jsdom",
     setupFiles: ["./setupTests.ts"],
     exclude: TEST_EXCLUDES,
     coverage: {
-      all: true,
       provider: "istanbul",
       exclude: COVERAGE_EXCLUDE,
       thresholds: {
